888d06c599872763aaac8248cf79a1b65208fcb2
Nicholas Bishop <nicholasbishop@gmail.com>
2019-05-20 08:47:17 -0400

    Change update callback signatures

--- src/lib.rs
+++ src/lib.rs
@@ -161,14 +161,14 @@ impl Collection {
     pub fn update_by_id<T, U>(&self, id: &Id, u: U) -> Result<(), DbError>
     where
         for<'de> T: Deserialize<'de> + Serialize,
-        U: Fn(&Id, &T) -> T,
+        U: Fn(&Item<T>) -> T,
     {
         let mut lock = FileLock::exclusive(&self.root)?;
         let path = self.item_path(id)?;
         let file = fs::File::open(&path)?;
         let reader = io::BufReader::new(file);
         let val = serde_json::from_reader(reader)?;
-        let val = u(&id, &val);
+        let val = u(&Item::new(id.clone(), val));
         let file = fs::File::create(&path)?;
         let writer = io::BufWriter::new(file);
         serde_json::to_writer(writer, &val)?;
@@ -179,8 +179,8 @@ impl Collection {
     pub fn update_many<T, F, U>(&self, f: F, u: U) -> Result<(), DbError>
     where
         for<'de> T: Deserialize<'de> + Serialize,
-        F: Fn(&Id, &T) -> bool,
-        U: Fn(&Id, &T) -> T,
+        F: Fn(&Item<T>) -> bool,
+        U: Fn(&Item<T>) -> T,
     {
         let mut lock = FileLock::exclusive(&self.root)?;
         for entry in fs::read_dir(&self.root)? {
@@ -194,8 +194,9 @@ impl Collection {
             let file = fs::File::open(&path)?;
             let reader = io::BufReader::new(file);
             let val = serde_json::from_reader(reader)?;
-            if f(&id, &val) {
-                let val = u(&id, &val);
+            let item = Item::new(id.clone(), val);
+            if f(&item) {
+                let val = u(&item);
                 let file = fs::File::create(&path)?;
                 let writer = io::BufWriter::new(file);
                 serde_json::to_writer(writer, &val)?;
